951e73837ee46b2d2d524dfeaea237d6c0488c75
Amir Khan <ak2022dev@gmail.com>
2022-06-26 09:04:55 +0100

    Passed first blank failing test to confirm jest setup / file require is operational

---
+++ searchCandies.js
@@ -0,0 +1,6 @@
+// File: searchCandies.js
+
+const searchCandies = (name,price) => {
+};
+
+module.exports = searchCandies;
---
+++ searchCandies.test.js
@@ -0,0 +1,8 @@
+// File: searchCandies.test.js
+
+const searchCandies = require('./searchCandies');
+
+describe( 'searchCandies', () => {
+  it( 'searches the candies object', () => {
+  });
+});
