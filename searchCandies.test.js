3e22276fdced905d165f28bb987fdd0316341df5
Amir Khan <ak2022dev@gmail.com>
2022-06-27 11:39:28 +0100

    Passed second test as-per required behaviour

--- searchCandies.test.js
+++ searchCandies.test.js
@@ -3,7 +3,10 @@
 const searchCandies = require('./searchCandies');
 
 describe( 'searchCandies', () => {
-  it( 'searches the candies object', () => {
+  it( 'returns the expected result searching with arguments ("Ma",10)', () => {
     expect(searchCandies('Ma', 10)).toEqual([ 'Mars', 'Maltesers' ]);
+  });
+  it( 'returns the expected result searching with arguments ("Ma",2)', () => {
+    expect(searchCandies('Ma', 2)).toEqual([ 'Mars' ]);
   });
 });
