ab5839940f5a6e99e67bc7ca967c5f178b7f6ec0
Amir Khan <ak2022dev@gmail.com>
2022-06-27 11:35:53 +0100

    Passed first failing test as-per required behaviour

--- searchCandies.test.js
+++ searchCandies.test.js
@@ -4,5 +4,6 @@ const searchCandies = require('./searchCandies');
 
 describe( 'searchCandies', () => {
   it( 'searches the candies object', () => {
+    expect(searchCandies('Ma', 10)).toEqual([ 'Mars', 'Maltesers' ]);
   });
 });
